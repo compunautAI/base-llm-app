acecac6403c67967c6c8f850efe75340865f4f0e
compunaut <voidstarcoder@gmail.com>
2025-07-25 14:06:33 -0500

    update ollama to use ollamaLLM

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
 
 def main():
     print("\n🤖 LLM Starter Agent (Ollama CLI)")
     print("Type 'exit' to quit.\n")
 
     # Initialize LLM
-    llm = Ollama(model="qwen2.5:1.5b")  # selects the model for ollama to use
+    llm = OllamaLLM(model="qwen2.5:1.5b")  # selects the model for ollama to use
     while True:
         user_input = input("You: ")
         if user_input.strip().lower() == "exit":
